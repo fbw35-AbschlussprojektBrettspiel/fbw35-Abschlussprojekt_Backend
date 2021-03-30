8cec57e0c00b3f8af5b4c36d7a744389d8df46a8
Christoph86 <ChrstphBch@web.de>
2021-03-30 21:59:14 +0200

    antorten-Array Prob gelöst, lösung für Postman als Kommentar

--- models/frage-model.js
+++ models/frage-model.js
@@ -5,10 +5,14 @@ const frageSchema = new Schema({
   thema: String,
   frage: String,
   // POST funktioniert nicht mit antworten als Array
-  // antworten: [String],
+  antworten: [String],
   indexRichtigeAntwort: Number,
   // evtl. für Später der Schwierigkeitsgrad
   // level: Number,
 });
 
 module.exports = mongoose.model('frage', frageSchema, 'fragen');
+
+//um einen Array in Postman abzusetzen benutzt man mehrfach den selben Feldnamen, dann wird's
+//ein Array sonst hält er es für einen langen einzelnen String, funktioniert!
+//  https://stackoverflow.com/questions/39667294/how-to-post-string-array-using-postman/39667496
