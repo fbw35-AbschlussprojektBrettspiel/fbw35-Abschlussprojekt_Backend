99278a4c86c2dcfcd2eaf92528e7a83ad1c55409
TunHuang <huang.tun.anton@gmail.com>
2021-05-04 11:48:41 +0200

    express wird nicht mehr gebraucht und jetzt aus dem projekt entfernt. index angepasst, controller entfernt, middleware entfernt, routes entfernt. nicht benötigte packete - express, express-validator, mongodb entfernt

--- middleware/corsMiddleware.js
+++
@@ -1,8 +0,0 @@
-const corsMiddleware = (request, response, next) => {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
-  response.header("Access-Control-Allow-Methods", "*")
-  next();
-};
-
-module.exports = corsMiddleware;
